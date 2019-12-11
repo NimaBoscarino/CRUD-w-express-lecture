a40cb3606f74f2532d90c46e67d0129079be60aa
Nima Boscarino <nima.boscarino@gmail.com>
2019-12-11 12:09:35 -0800

    user auth dec 12

--- server.js
+++ server.js
@@ -2,14 +2,47 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const cookieSession = require('cookie-session')
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true })) // this gives us req.body
+app.use(cookieParser())
+app.use(cookieSession({
+    name: 'session',
+    keys: ['ayy', 'what', 'up', 'my', 'dudes', 'it is wednesday'],
+  
+    // Cookie Options
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+  }))
+  
+// bcrypt
+
+// Nima's Cookie Parser
+// app.use((req, res, next) => {
+//     const cookieString = req.headers.cookie
+//     const cookies = cookieString.split('; ').map(c => c.split('='))
+//     // console.log(cookies)
+
+//     req.nima_cookies = cookies
+//     next()
+// })
+
+
 
 const users = {
-    '5cu2t': 'Nima Boscarino',
-    '7bn18s': 'Bob Bobson',
-    '7an01': 'Bradlina Kristoferrettason'
+    '5cu2t': {
+        name: "nima",
+        password: "potatoes"
+    },
+    '7bn18s': {
+        name: "bob",
+        password: "bob123"
+    },
+    '7an01': {
+        name: "bradlina",
+        password: "tomato"
+    },
 }
 
 const blogs = {
@@ -31,8 +64,17 @@ const blogs = {
 }
 
 app.get('/', (req, res) => {
+    console.log('COOKIE PARSER', req.cookies)
+    console.log('SESSION', req.session)
+    // const user = req.nima_cookies[0][1]
+    // const user = req.cookies.user || "Anonymous"
+    const user = req.session.user || "Anonymous"
+
+    req.session.hello = 'World'
+
     let templateVars = {
-        blogs: blogs
+        blogs: blogs,
+        user: user
     }
 
     res.render('index', templateVars)
@@ -89,6 +131,46 @@ app.post('/blogs/:id/delete', (req, res) => {
 
     res.redirect("/")
 })
+
+app.get('/see-cookies', (req, res) => {
+    // console.log(req.cookies)
+    // console.log(res.cookie())
+    res.cookie("Nima", "LKAHJSGDKJAHSGDKJHASGDKAJSD")
+    res.cookie("Bradlina", "Hello world")
+
+    res.send("HASDHASHDHASD")
+})
+
+app.get('/login/:user', (req, res) => {
+    res.cookie("user", req.params.user)
+
+    res.send("Logged in!")
+})
+
+
+app.post('/login', (req, res) => {
+    // use the form data to set the cookie
+    const userName = req.body.user
+    const password = req.body.password
+
+    let foundUser = null
+    // check if password matches username
+    for (const userId in users) {
+        if (users[userId].name === userName && users[userId].password === password) {
+            foundUser = users[userId]
+        }
+    }
+
+    if (foundUser) {
+        // res.cookie("user", foundUser.name)
+        req.session.user = foundUser.name
+        res.redirect('/')
+    } else {
+        res.send('bad login')
+    }
+
+})
+
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Listening on ${PORT}`)
